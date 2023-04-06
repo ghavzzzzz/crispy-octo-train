e69b5066ab323ed0184808640fa8d25144d6c365
ghavzzzzz <101458773+ghavzzzzz@users.noreply.github.com>
2023-04-06 12:50:42 +0200

    Add files via upload

---
+++ BR.js
@@ -0,0 +1,29 @@
+sta="";
+function preload(){
+
+    img = loadImage('phone laptop.jpg');
+}
+function setup(){
+
+    canvas=createCanvas(640 , 420);
+    canvas.center();
+    co= ml5.objectDetector("cocossd" , modelLoaded);
+    document.getElementById("Od").innerHtTML="Status :Dedecting object.";
+    video=createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+function modelLoaded(){
+
+    console.log('Model Loaded $#!*&%');
+    sta=true;
+    co.detect(img , gotResult);
+}
+function gotResult (error , results){
+    if (error){
+       console.log(error);
+    }
+    else{
+        console.log(results);
+    }
+} 
